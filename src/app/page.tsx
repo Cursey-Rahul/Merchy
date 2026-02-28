884fee5c27b92af86fc1d996c3ba2ebed1093ae9
Cursey-Rahul <cursey.pvt.ltd@gmail.com>
2026-02-28 18:19:51 +0530

    final fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Featured from "@/components/Featured";
 import Offer from "@/components/Offer";
 import Slider from "@/components/Slider";
 import Toss3d from "@/components/Toss3D";
+export const dynamic = 'force-dynamic';
+
 
 
 export default function Home() {
